786c8dc4e0c0f41422aa953786c3e97f10504254
Willian Franco <ing.wfranco@gmail.com>
2022-05-25 08:31:10 -0500

    first commit

---
+++ src/main/java/katas/Kata5.java
@@ -0,0 +1,24 @@
+package katas;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import model.BoxArt;
+import model.Movie;
+import model.MovieList;
+import util.DataUtil;
+
+import java.util.List;
+import java.util.Map;
+
+/*
+    Goal: Retrieve the largest rating using reduce()
+    DataSource: DataUtil.getMovies()
+    Output: Double
+*/
+public class Kata5 {
+    public static Double execute() {
+        List<Movie> movies = DataUtil.getMovies();
+
+        return 3.0;
+    }
+}
